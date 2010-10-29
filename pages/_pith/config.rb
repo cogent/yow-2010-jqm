bfc3f4cdb840c0c32ec0ba4c3977fcc63dece464
iain <iain@mccoy.id.au>
2010-10-29 19:36:29 +1100

    work with the new data format

--- pages/_pith/config.rb
+++ pages/_pith/config.rb
@@ -3,9 +3,12 @@ project.assume_directory_index = true
 
 project.helpers do
   
-  def yow_days
-    yow_yaml = File.expand_path("../../../yow.yaml", __FILE__)
-    YAML.load(File.open(yow_yaml))
+  def yow_yaml
+    @yow_yaml ||= File.expand_path("../../../yow.yaml", __FILE__)
   end
-  
+
+  def yow_days
+    YAML.load(File.open(yow_yaml))[:days]
+  end
+
 end
