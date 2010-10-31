37c9aaae9b79bc5cab63aa0907cd3fb0a3a2b10a
Marty Andrews <marty@cogentconsulting.com.au>
2010-10-31 22:31:47 +1100

    Add an alphabetical list of sessions and speakers

--- pages/_pith/config.rb
+++ pages/_pith/config.rb
@@ -12,7 +12,13 @@ project.helpers do
   end
 
   def yow_sessions
-    yow_days.map { |day| day.map { |timeslot| timeslot[:sessions] } }.flatten
+    sessions = yow_days.map { |day| day.map { |timeslot| timeslot[:sessions] } }.flatten
+    sessions.sort {|x,y| x[:title] <=> y[:title]}
+  end
+
+  def yow_speakers
+    speakers = YAML.load(File.open(yow_yaml))[:speakers].values
+    speakers.sort {|x,y| x['name'] <=> y['name']}
   end
 
 end
